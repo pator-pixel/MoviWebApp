d5ebaad248bc13e81fc80edfd1f3124046e0b12b
pator-pixel <pator@gmx.net>
2026-06-07 14:18:30 +0200

    routen verknüpft

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from data_manager import DataManager
-from models import db, Movie
+from models import db, Movie, User
 
 app = Flask(__name__)
 
@@ -28,13 +28,58 @@ def create_user():
     return redirect(url_for('index'))
 
 
-@app.route('/users/<int:user_id>')
+@app.route('/users/<int:user_id>/movies', methods=['GET', 'POST'])
 def user_movies(user_id):
-    return f"Movies for user {user_id}"
+    user = User.query.get(user_id)
+
+    if user is None:
+        return "User not found", 404
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        director = request.form.get('director')
+        year = request.form.get('year')
+        poster_url = request.form.get('poster_url')
+
+        if name:
+            movie = Movie(
+                name=name,
+                director=director,
+                year=int(year) if year else None,
+                poster_url=poster_url,
+                user_id=user_id
+            )
+            data_manager.add_movie(movie)
+
+        return redirect(url_for('user_movies', user_id=user_id))
+
+    movies = data_manager.get_movies(user_id)
+    return render_template('movies.html', user=user, movies=movies)
+
+
+@app.route('/users/<int:user_id>/movies/<int:movie_id>/update', methods=['POST'])
+def update_movie(user_id, movie_id):
+    new_title = request.form.get('name')
+
+    if new_title:
+        data_manager.update_movie(movie_id, name=new_title)
+
+    return redirect(url_for('user_movies', user_id=user_id))
+
+
+@app.route('/users/<int:user_id>/movies/<int:movie_id>/delete', methods=['POST'])
+def delete_movie(user_id, movie_id):
+    data_manager.delete_movie(movie_id)
+    return redirect(url_for('user_movies', user_id=user_id))
 
 
 with app.app_context():
     db.create_all()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
